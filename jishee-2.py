c1e4bd73d92cf0aeeff3453bc35f21525653cdce
Bayrmagnai99461124 <bayrmagnai.1124@gmail.com>
2025-01-29 13:42:19 +0800

    magnai

---
+++ jishee-2.py
@@ -0,0 +1,13 @@
+course_id = input("хичээлийн код: ")
+course_name = input("хичээлийн нэр: ")
+credit = input("хичээлийн кредит: ")
+
+madlib = "энэ хичээлийн код нь {}".format(course_id).upper() + \
+    ", хичээлийн нэр нь {}".format(course_name).title() + \
+    "бөгөөд уг хичээл нь {}".format(credit) + "  кредитийн хичээл юм."
+
+madlib1 = " энэ хичээлийн код нь {}".format(course_id).upper() + \
+    ", хичээлийн нэр нь {}".format(course_name).title() + \
+    "бөгөөд уг хичээл нь {}".format(credit) + "  кредитийн хичээл юм."
+print(madlib)
+print(madlib1)
